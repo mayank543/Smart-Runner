a1d4e192a6e1ce176fc25938cd7f21ee1e779755
mayank543 <mayankdoholiya@gmail.com>
2025-08-08 19:55:53 +0530

    sevrer

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ import { requireAuth } from './middleware/requireAuth.js';
 
 dotenv.config();
 
-const app = express();
+ const app = express();
 
 const corsOptions = {
   origin: ['http://localhost:5173','https://smart-runner.vercel.app'], // Your frontend URL
